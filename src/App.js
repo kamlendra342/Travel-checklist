a92406f42046a212cc98333f9c9a2497c5bfc6f8
Kamlendra <tirupatibalaj789@gmail.com>
2024-04-11 15:22:21 +0530

    Intial commit

--- src/App.js
+++ src/App.js
@@ -117,7 +117,7 @@ function Stats({ packedItems, noOfItems }) {
     return (
     <footer className="stats">
         <em>
-        {noOfItems==0 ? "Start adding some items to your PackingList 👗👖" : percentage == 100 ? "You got everyThing ! Ready to Go..  ✈️✈️" : `🛍️  You have ${noOfItems} items in you list , and you alredy packed ${packedItems} (${percentage}%)`}
+        {noOfItems === 0 ? "Start adding some items to your PackingList 👗👖" : percentage === 100 ? "You got everyThing ! Ready to Go..  ✈️✈️" : `🛍️  You have ${noOfItems} items in you list , and you alredy packed ${packedItems} (${percentage}%)`}
       </em>
     </footer>
   )
